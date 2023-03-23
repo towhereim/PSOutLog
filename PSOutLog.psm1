5a737bd03337649980ba87439cba2d3c19341032
shlim <shlim@zenithn.com>
2023-03-23 11:33:17 +0900

    Just Out-Log "text". This is PowerShell module that makes PowerShell logging easy.

---
+++ PSOutLog.psm1
@@ -0,0 +1,13 @@
+$logColor=@{"[INFO]"="Green";"[WARN]"="Yellow";"[ERR]"="Red"}
+function Add-Logging ([string]$str, [string]$lvl){
+    $psPath = (Get-PSCallStack)[2].ScriptName
+    $path = $psPath ? $psPath.Replace("/"+$psPath.Split('/')[-1],"") : "."
+    $logPath="$path/logs/$($psPath.Split('/')[-1].Split('.ps1')[0]).log"
+    if(!(Test-Path $logPath)){New-Item $logPath -Force}
+    $logStr="$(Get-Date -Format "yyyy-MM-dd HH:mm:ss.fff")`t$lvl $str"
+    Write-Host $logStr -ForegroundColor:$logColor[$lvl]
+    Add-Content -Path $logPath -Value $logStr
+}
+function Out-Log ([string]$str){Add-Logging $str "[INFO]"}
+function Out-WarnLog ([string]$str){Add-Logging $str "[WARN]"}
+function Out-ErrLog ([string]$str){Add-Logging $str "[ERR]";exit; }
